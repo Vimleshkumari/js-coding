907bc28f435c3ba4c89d379047da9bdb8dd69338
Vimlesh kumari <vimlesh3012@gmail.com>
2025-06-27 22:49:29 +0530

    js quetions

--- instance.js
+++ instance.js
@@ -9,21 +9,31 @@ console.log(obj);
 console.log(obj2)
 
 console.log([] == "");   //true when compare two different type [] converted tp empty string
-console.log([]== [])  //false when compare two array, it refer to different references
+console.log([] == [])  //false when compare two array, it refer to different references
 
 
 
-var foo="lion";
-function show(){
+var foo = "lion";
+function show() {
     foo = "panda";
     return;
-    function foo(){}
+    function foo() { }
 }
 // console.log(show());
 show()
 console.log(foo);
 
 //in js function or var with same name override each other
+
+let str = "code js and code only do code in js"
+let strarray= str.split("")
+let result =strarray.reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1
+    return acc
+
+}, {})
+console.log("result",result);
+
 
 
 
