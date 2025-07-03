625bd5eae0abe4eeab72f05df51c317faa603475
Vimlesh kumari <vimlesh3012@gmail.com>
2025-07-03 23:49:12 +0530

    anagram code added

---
+++ anagram.js
@@ -0,0 +1,7 @@
+const isAnagram = (str1, str2) => {
+    console.log(str1.split("").sort());
+    
+    return str1.split("").sort().join("") === str2.split("").sort().join("")
+}
+
+console.log("Is Anagram", isAnagram("triab", "itrm"));
