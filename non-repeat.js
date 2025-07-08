5ccead0d06b91b27ea2a238a146d8d6e10d5ad42
Vimlesh kumari <vimlesh3012@gmail.com>
2025-07-08 22:05:56 +0530

    repeat frequency added

--- non-repeat.js
+++ non-repeat.js
@@ -1,14 +1,23 @@
 const firstNonRepeatingChar = (str) => {
     let strarr = str.split("")
-    let result = []
-    let rslt = strarr.reduce((acc, ele) => {
-        acc[ele] = !acc[ele] ? 1 : acc[ele] + 1
-        result.push(acc)
-        return acc
-    }, {})
+    let result = {}
+    // let rslt = strarr.reduce((acc, ele) => {
+    //     acc[ele] = !acc[ele] ? 1 : acc[ele] + 1
+    //     result.push(acc)
+    //     return acc
+    // }, {})
 
-    console.log(rslt)
+    for (let ele of str) {
+        result[ele] = (result[ele] || 0) + 1
+    }
+    for (let item of str) {
+        if (result[item] === 1) {
+            return item
+        }
+    }
+    return null
+    //{ a: 2, b: 2, c: 2, d: 2, e: 2, f: 1 }
 }
-firstNonRepeatingChar("aabbccddeef") // returns "f"
-// firstNonRepeatingChar("aabb")        // returns null
-// firstNonRepeatingChar("swiss")
+console.log(firstNonRepeatingChar("aabbccddeefaaag"))// returns "f"
+console.log(firstNonRepeatingChar("aabb"));  // returns null
+console.log(firstNonRepeatingChar("swiss"));
