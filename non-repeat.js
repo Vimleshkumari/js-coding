852554c37fa5fe412522a700c67bff7d8760b435
Vimlesh kumari <vimlesh3012@gmail.com>
2025-07-10 09:47:09 +0530

    repeat frequency added

--- non-repeat.js
+++ non-repeat.js
@@ -10,6 +10,7 @@ const firstNonRepeatingChar = (str) => {
     for (let ele of str) {
         result[ele] = (result[ele] || 0) + 1    //{ a: 2, b: 2, c: 2, d: 2, e: 2, f: 1 }
     }
+    
     for (let item of str) {
         if (result[item] === 1) {
             return item
