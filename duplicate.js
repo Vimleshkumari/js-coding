85f72bfa26488b56118dd886034c1828974c54bf
Vimlesh kumari <vimlesh3012@gmail.com>
2025-07-24 22:59:37 +0530

    remove duplicate

--- duplicate.js
+++ duplicate.js
@@ -1,5 +1,12 @@
 
 
 const arr = [1, 2, 2, 3, 1];
-const unique = {...new Set(arr)}; // [1, 2, 3]
-console.log(unique);
+const unique = { ...new Set(arr) }; // [1, 2, 3]
+
+let uniquearr = []
+arr.forEach(ele => {
+    if (!uniquearr.includes(ele)){
+        uniquearr.push(ele)
+    }
+})
+console.log(uniquearr);
